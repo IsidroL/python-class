450f98284ea2bdc991d45d5c7f985ab88a6aa68e
Isidro Lucatero <isidrolojr@gmail.com>
2023-11-21 20:36:55 -0800

    add changes

--- json-class.py
+++ json-class.py
@@ -12,3 +12,17 @@ data= {
     'interests': ['golf','running','videogames'],
     'is_student': False
 }
+
+
+# Writing data to a json file
+with open('data.json','w') as json_file:
+
+    json.dump(data,json_file,indent=4)
+
+print('Data written to data.json.')
+
+
+# Reading data from a JSON file 
+with open('data.json', 'r') as json_file:
+
+    loaded_data = json.load
